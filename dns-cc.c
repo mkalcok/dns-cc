925b63dcab554e3031c11860d62e784276839073
kalcok <martin.kalcok@gmail.com>
2013-12-12 03:34:54 +0100

    Parse dig output

--- dns-cc.c
+++ dns-cc.c
@@ -7,12 +7,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    system("ping 8.8.8.8");
+   
+    char s[100], substring[13], check[13]=";; Query time", *time, *units;
+    int i = 0, milisec=0;
+    FILE *input;
+
+    input = popen("dig @208.67.222.222 www.fasjdaslkmkls.sk +time=10", "r");
+    while(fgets(s, 100, input) != NULL){
+        if(strcmp(strncpy(substring, s, 13), check) == 0){
+            strcpy(substring, s+15);
+            time = strtok(substring, " ");
+            units = strtok(NULL, " ");
+            break;
+        }
+    }
+    milisec = strtol(time, NULL, 0);
+    if(strcmp(units, "s") == 0){
+        milisec *= 1000;
+    }
+    printf("%d\n",milisec);
     return (EXIT_SUCCESS);
 }
 
