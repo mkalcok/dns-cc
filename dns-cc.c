02d08def437108fce8b2fe6ccc608e2dab45991d
kalcok <martin.kalcok@gmail.com>
2013-12-12 01:07:18 +0100

    Initial commit

---
+++ dns-cc.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: kauchman
+ *
+ * Created on December 11, 2013, 9:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    system("ping 8.8.8.8");
+    return (EXIT_SUCCESS);
+}
+
