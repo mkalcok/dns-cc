3e8d2b62c1e441f8464363a2474009fa4ab732af
kalcok <martin.kalcok@gmail.com>
2013-12-12 13:05:19 +0100

    Added iscached() function

--- dns-cc.c
+++ dns-cc.c
@@ -9,30 +9,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 
 /*
  * 
  */
-int main(int argc, char** argv) {
-   
+struct dataByte{
+   unsigned short i[8];
+};
+int getDelay(char *server, char *name){
+    
     char s[100], substring[13], check[13]=";; Query time", *time, *units;
     int i = 0, milisec=0;
     FILE *input;
-
-    input = popen("dig @208.67.222.222 www.fasjdaslkmkls.sk +time=10", "r");
+    input = popen("dig @208.67.222.222 www.fasjdaasdasdslkmkls.sk", "r");
     while(fgets(s, 100, input) != NULL){
-        if(strcmp(strncpy(substring, s, 13), check) == 0){
+        if(strncmp(strncpy(substring, s, 13), check, 13) == 0){
             strcpy(substring, s+15);
             time = strtok(substring, " ");
             units = strtok(NULL, " ");
             break;
         }
     }
+    pclose(input);
     milisec = strtol(time, NULL, 0);
     if(strcmp(units, "s") == 0){
         milisec *= 1000;
     }
-    printf("%d\n",milisec);
+    return milisec;
+}
+
+int isCached(char *server, char *name){
+    int i, initial, control[5], treshold, cached;
+    initial = getDelay(server, name);
+    treshold = initial / 5;
+        printf("initial: %d\ntreshold: %d\n",initial, treshold);
+    for(i = 0; i<(sizeof(control) / sizeof(control[0])); i++){
+        control[i] = getDelay(server, name);
+        printf("control[%d]: %d\n",i , control[i]);
+        if (control[i] < (initial - treshold)){
+            cached = 0;
+        }else{cached = 1;}
+    }
+    return cached;
+}
+
+int main(int argc, char** argv) {
+    
+    
+    printf("Is cached?: %d\n", isCached("lorem","ipsum"));
+    //printf("%lu\n",sizeof(n));
     return (EXIT_SUCCESS);
 }
 
