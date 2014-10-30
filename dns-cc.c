8781bb44a456fd30aac1a68cb9b9ae649c30ba73
Kalcok <martin.kalcok@gmail.com>
2014-10-30 17:25:54 +0100

    DP is back :P (Stream processing, byte processing, bit classification (mknn) and what not)

--- dns-cc.c
+++ dns-cc.c
@@ -9,27 +9,68 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include "compress.h"
+//#include <ares.h>
 /*
  * 
  */
+
+//Define argument flags
+#define CONF_FLAG  1
+#define SEND_FLAG  2
+#define READ_FLAG  4
+#define COMPRESS_FLAG  8
+
+typedef unsigned char     uint8_t;
+typedef unsigned short    uint16_t;
+
+static long bitcounter_g = 0;
+
+//Config declarations
+typedef struct conf{
+    char *conf_file;
+    char *input_file;
+    char *output_file;
+    char *name_base;
+    char *name_server;
+    char *key;
+    int precision;
+    } conf;
+
+static struct conf *config;
+
+typedef struct sample{
+    int *cached_set;
+    int *uncached_set;
+    } sample;
+
+static struct sample *sample_times;
 
 int _pow(int base, int exp){                                                    /*rekurzivna funkcia na vypocet mocniny base^exp*/
     return exp == 0 ? 1 : base * _pow(base, exp - 1); 
 }
 
-void getname(char *output, char *key, int seq){                                 /*Funkcia generuje dns meno, bude komplexnejsia zatial je to len zakladny navrh kvoli testu funkcnosti*/
+float absolute_value(float a){
+    if(a < 0){
+        a = a*(-1);
+    }
+    return(a);
+}
+
+void compose_name(char *output, int seq){                                 /*Funkcia generuje dns meno, bude komplexnejsia zatial je to len zakladny navrh kvoli testu funkcnosti*/
     char charseq[10];                                                           /*inicializacia uvodnych premennych*/
-    char domain2[10] = "www.";
-    char tld[5] = ".sk";
     sprintf(charseq, "%d", seq);                                                /*sekvencne cislo ktore bolo ako argument funkcie sa konvertuje int -> string*/
     
-    strcat(output, domain2);                                                    /*strcat posklada vysledne meno*/
-    strcat(output, key);
+    strcat(output, config->key);
     strcat(output, charseq);
-    strcat(output, tld);
-    
+    strcat(output, config->name_base);
+    int i =0;
     return;
 }
+
+
 void bintostr(char *output, char *binstring){                                                /*funkcia transformuje binarny retazec do stringu*/
     int i, y = 0, power, dec;                                                   /*inicializacia premennych*/
     char toparse[1];
@@ -50,59 +91,117 @@ void bintostr(char *output, char *binstring){                                   
     return;
 }
 
-void readmsg(char *key){							/*Funkcia  cita spravu z dns mien odvodenych z char *key */
+void retrieve_msg(){
+    int i, y= 0, endofmsg = 0;
+    char bit[2];
+    FILE *fp = fopen(config->output_file,"wb");
+    int byte = 0;
+    char *name = calloc(255, sizeof(char));
+    while (endofmsg != 1){
+        byte = 0;
+	endofmsg = 1;
+        for(i = y; i < y+8; i++){
+            byte = byte << 1;
+            strcpy(name, "");
+            compose_name(name ,i);
+            if (iscached(config->name_server, name)){
+                byte |= 1;
+                endofmsg =0;
+            }
+        }
+        fwrite(&byte, 1, 1, fp);
+        y += 8;
+    }
+    free(name);
+    fclose(fp);
+    return;
+}
+
+
+
+void retrieve_msg2(){	        						/*Funkcia  cita spravu z dns mien odvodenych z char *key */
     int i, y= 0, endofmsg = 0;							/*inicializacia uvodnych premennych*/
     char bit[2];
-    char *binmessage = (char *) malloc(1600);
-    char *name = (char *) malloc((sizeof(char)) * 255);
-    char *message = (char *) malloc(200);
+    char *binmessage = calloc(16000, sizeof(char));
+    char *name = calloc(255, sizeof(char));
+    char *message = calloc(200, sizeof(char));
     while (endofmsg != 1){							/*While cyklus prechadza jednotlive bajty spravy zatial co vnoreny for cyklus prechadza tieto bajty po bitoch, opakuje sa az kym for cyklus nenajde bajt plny nulovych bitov (sprava skoncila)*/
 	endofmsg = 1;								/*na zaciatku kazdeho cyklu predpokladame ze tento bajt je posledny*/
         for(i = y; i < y+8; i++){    						/*for cyklus prejde 8 bitov ktore zapise do vystupneho binarneho retazca binstring, ak narazi na '1' zapise do premennej endofmsg hodnotu 0 cim indikuje ze nejde o posledny bajt*/
             strcpy(name, "");                                                   /*vynulovanie retazca pri kazdej iteracii cyklu*/
-            getname(name ,key,i);                                               /*z kluca a cisla iteracie vygenerujeme plne dns meno funkciou getname()*/
-            sprintf(bit, "%d", iscached("208.67.222.222", name));		/*DNS zaznam sa otestuje ci je zacacheovany a vysledok sa pretypuje int -> string*/
-            //printf("%s %c \n",name, bit[0]);
+            compose_name(name ,i);                                               /*z kluca a cisla iteracie vygenerujeme plne dns meno funkciou compose_name()*/
+            sprintf(bit, "%d", iscached(config->name_server, name));		/*DNS zaznam sa otestuje ci je zacacheovany a vysledok sa pretypuje int -> string*/
+            //printf("%c",bit[0]);
             strcat(binmessage, bit);						/*vysledok sa prilepy na koniec retazca binmessage*/
 	    if(bit[0] == '1'){							/*ak je vysledok '1', indikuje to ze nejde o posledny bajt a podla toho sa nastavi aj premenna endofmsg */
 	    endofmsg =0;
 	    }
         }
-   y += 8; 									/*pocitadlo sa posunien a zaciatok dalsieho bajtu*/
-   }
-   bintostr(message, binmessage);						/*sprava sa posle funkcii bintostr() aby sa prelozil bitovy retazec na text a vypise sa*/
-   printf("%s \n",message);
-   free(binmessage);
-   free(name);
-   free(message);
-   return;										/*uvolni sa alokovana pamat*/
+    //printf(" ");
+    y += 8; 									/*pocitadlo sa posunien a zaciatok dalsieho bajtu*/
+    }
+    bintostr(message, binmessage);						/*sprava sa posle funkcii bintostr() aby sa prelozil bitovy retazec na text a vypise sa*/
+    printf("%s \n",message);
+    free(binmessage);
+    free(name);
+    free(message);
+    return;										/*uvolni sa alokovana pamat*/
 }
 
-void sendmsg(char *binmessage, char *key ){					/*vunkcia zapise bitovu spravu binmessage na DNS server podla kluca key*/
-    int i;									/*inicializacia uvodnych premennych*/
-    char bit[1];
-    char cmd[25] = "dig @208.67.222.222 ";					/*samotny prikaz*/
+void * bulk_test(void * q){
+    printf("%s\n",(char *) q);
+    //system(q);
+    return;
+}
+
+void send_data(int byte){   
+    pthread_t threads[8] = {0};
+    int ret_t[8];    					/*vunkcia zapise bitovu spravu binmessage na DNS server podla kluca key*/
+    int indexes[8];
+    int i,z;
+    int j=128;
+    char space[2] = " \0";
+    char cmd[25] = "dig @";             					/*samotny prikaz*/
     char pipe[15] = " > /dev/null";						/*pipe sa prilepy nakoniec aby sme sa zbavily textoveho vystupu programu*/
-    char *query = (char *) malloc((sizeof(char)) * 255);
-    
-    for(i = 0; i < strlen(binmessage); i++){					/*binmessage sa prechadza po bitoch a kde sa narazi na hodnotu jedna tam sa spravi DNS dotaz*/
-        strcpy(query, cmd);
-        strncpy(bit, binmessage + i, 1);
-        if(bit[0] == '1'){
-            getname(query, key, i);
-	    strcat(query, pipe);
-            system(query);
-        }
+    char **query = malloc(8*(sizeof(char *))); 
+    for(i = 0; i < 8; i++){
+        query[i]= calloc(255, sizeof(char));
+
     }
-   free(query);
-   return;
+    for(i = 0; i < 8; i++){					/*binmessage sa prechadza po bitoch a kde sa narazi na hodnotu jedna tam sa spravi DNS dotaz*/
+        strcpy(query[i], cmd);
+        strcat(query[i], config->name_server);
+        strcat(query[i], space);
+        if(byte & j){
+            compose_name(query[i], bitcounter_g);
+	    strcat(query[i], pipe);
+            indexes[i] = i;
+            //pthread_create(&threads[i], NULL, bulk_test,&i);
+            pthread_create(&threads[i], NULL, bulk_test , query[i]);
+            //system(query);
+        }
+        j = j >> 1;
+        ++bitcounter_g;
+    }
+    //printf("\r%ld bytes sent",(bitcounter_g/8));
+    //fflush(stdout);
+    
+    for(z=0;z < 8; z++){
+        pthread_join(threads[z], NULL);
+    }
+
+    for(i = 0; i < 8; i++){
+        free(query[i]);
+    }
+    free(query);
+    
+    return;
 }
 
 void strtobin(char *output, char *input){                                                    /*funkcia transformuje string na binarny retazec*/
     int i, y, dec, bin;                                                             /*inicializacia premennych*/
     char toparse[1];
-
-    for(i = 0; i < strlen(input) -1; i++){                                          /*cyklus prechadza string po znakoch*/
+    for(i = 0; i < strlen(input)-1; i++){                                          /*cyklus prechadza string po znakoch*/
         strncpy(toparse, input + i, 1);                                         /*vyber jedneho pismena*/
         dec = toparse[0];                                                       /*char to int*/
         //printf("%c - %d - ", toparse[0], dec);        
@@ -123,6 +222,8 @@ void strtobin(char *output, char *input){                                       
    return;
 }
 
+
+//TODO:Get server and name from config!
 int getdelay(char *server, char *name){                                         /*funkcia vyparsuje delay odpovede DNS servera*/
     
     char s[100], substring[13], check[13]=";; Query time", *time, *units;       /*inicializacia uvodnych premennych*/
@@ -150,56 +251,246 @@ int getdelay(char *server, char *name){                                         
     if(strcmp(units, "s") == 0){                                                /*prevod jednotiek v pripade ze je delay v sekundach*/
         milisec *= 1000;
     }
+    //printf("%d", milisec);
     return milisec;
 }
 
-int iscached(char *server, char *name){                                         /*Funkcia zisti na zaklade odozvy odpovede ci je zaznam v cache*/
-    int i, initial, control[5], treshold, cached;                               /*inicializacia premennych. i - pomocna premenna cyklu; initial - delay prveho dotazu; control[] - delay x kontrolnych dotazov; treshhold - odchylka control[] od initial ktora urcuje pritomnost v cache; cached - 0 alebo 1, hodnota je navratovou hodnotou funkcie*/
-    
-    initial = getdelay(server, name);                                           /*ziskanie prveho delayu*/
-    treshold = initial / 5;                                                     /* threshhold je 20% z initial*/
-    
-    //printf("initial: %d\ntreshold: %d\n",initial, treshold);
-    for(i = 0; i<(sizeof(control) / sizeof(control[0])); i++){
-        control[i] = getdelay(server, name);                                    /*ziskanie kontrolneho delayu*/
-        //printf("control[%d]: %d\n",i , control[i]);
-        if (control[i] < (initial - treshold)){                                 /*kontrola ci je control[] mensi ako initial minus treshhold*/
-            cached = 0;
-        }else{cached = 1;}
+//Modified kNN algorithm
+int iscached(char *server, char *name){
+    int delay = getdelay(server, name);
+    float distance = 0;
+    float cached_score = 0;
+    float uncached_score = 0;
+    int i;
+
+    for(i=0; i < config->precision; i++){
+        if(delay == sample_times->cached_set[i]){continue;}
+        distance = 1.0/(delay - sample_times->cached_set[i]);
+        cached_score += absolute_value(distance);
+
+        if(delay == sample_times->uncached_set[i]){continue;}
+        distance = 1.0/(delay - sample_times->uncached_set[i]);
+        uncached_score += absolute_value(distance);
     }
-    return cached;
+    if(cached_score > uncached_score){
+        return(1);
+    }else{
+        return(0);
+    }
+    
+}
+
+
+void* set_member(void  *value, size_t size){
+    void* p = calloc(size+1, sizeof(char));
+    strncpy(p, value, size);
+    return p;
+}
+
+conf* init_conf(){
+    conf* c = malloc(sizeof(conf));
+    c->conf_file = set_member("/etc/dns-cc/dns.cfg", sizeof("/etc/dns-cc/dns.cfg"));
+    return c;
+}
+
+void free_conf(struct conf *p){
+    free(p->conf_file);
+    free(p->input_file);
+    free(p->output_file);
+    free(p->name_base);
+    free(p->name_server);
+    free(p->key);
+    free(p);
+    return;
+}
+
+void help(){
+    printf("\nOne day, this will be nice help :)\n");
+}
+
+void remove_blank(char *str){
+    char *clean_str = calloc(strlen(str), sizeof(char));
+    char *buf = malloc(1);
+    int i = 0;
+    while(1){
+        memcpy(buf, &str[i], 1);
+        if(strcmp(buf, "\0") == 0){break;}
+        if(iscntrl(buf[0]) != 0){++i; continue;}
+        if(isblank(buf[0]) == 0){strncat(clean_str, buf, 1);}
+        ++i;
+    }
+    strcat(clean_str, "\0");
+    strcpy(str, clean_str);
+    free(clean_str);
+    free(buf);
+    return;
+}
+
+void set_conf(char* var, char* value){
+    int value_length = (int)strlen(value);
+    if(strcasecmp(var, "server") == 0){
+        config->name_server = set_member(value, value_length);
+        return;
+    }
+    if(strcasecmp(var, "base_name") == 0){
+        config->name_base = set_member(value, value_length);
+        return;
+    }
+    if(strcasecmp(var, "key") == 0){
+        config->key = set_member(value, value_length);
+        return;
+    }
+    if(strcasecmp(var, "precision") == 0){
+        config->precision = atoi(value);
+        return;
+    }
+
+
+
+}
+
+void read_conf_file(){
+    FILE *fp;
+    fp = fopen(config->conf_file, "r");
+    size_t line_length = 200;
+    char *var;
+    char *line = malloc(line_length);
+    ssize_t read;
+
+    if (fp == NULL){
+        printf("Can't open configuration file. %s\n", config->conf_file);
+        exit(0);
+    }
+
+    printf("Using config file %s\n", config->conf_file);
+    
+    while ( (read = getline(&line, &line_length, fp)) != -1){
+    if(strncmp(line,"#",1) == 0 || strncmp(line, "\n", 1) == 0){continue;}
+    line_length = (int)strlen(line);
+    
+    remove_blank(line);
+    char *p = strdup(line);
+    var = strsep(&p, "=");
+    set_conf(var, p);
+    //Freeing var because it points to original *p where space was allocated by strdup
+    free(var);
+    }
+    fclose(fp);
+    free(line);
+}
+
+sample* init_sample_times(){
+    sample *s = malloc(sizeof(sample));
+    s->cached_set = calloc(config->precision, sizeof(int));
+    s->uncached_set = calloc(config->precision, sizeof(int));
+}
+
+void free_sample_times(){
+    free(sample_times->cached_set);
+    free(sample_times->uncached_set);
+    free(sample_times);
+}
+
+
+void calibrate(){
+    sample_times = init_sample_times();
+    int i = 0;
+    char *name = calloc(255, sizeof(char));
+    for(i; i < config->precision; i++){
+        strcpy(name, "precise.\0");
+        compose_name(name, i);
+        sample_times->uncached_set[i] = getdelay(config->name_server, name);
+        sample_times->cached_set[i] = getdelay(config->name_server, name);
+    }
+    free(name);
+    return;
 }
 
 int main(int argc, char** argv) {
-    int c;
-    char message[140], name[250];
+    int c, option_flags=0;
+    config = init_conf();
+    //XXX:Dprecated config = malloc(sizeof(conf));
+    char ch[1];
     FILE *fp;
-    while((c = getopt(argc, argv, "sr")) != -1){
+    char cfg_file[100];
+    while((c = getopt(argc, argv, "Cc:s:r:")) != -1){
         switch (c){
+            case 'c':
+                //Redefine config file
+                free(config->conf_file);
+                config->conf_file = set_member(optarg, strlen(optarg));
+                break;
+
+            case 'C':
+                //Are we gonna compress?
+                option_flags += COMPRESS_FLAG;
+                break;
+    
             case 's':
-   	        fp = fopen("sample.msg", "r");
-    	        if (fp == NULL){
-       		    printf("cant open file\n");
-            	    return(0);
-   	    	}	
-    
-    	  	while(fgets(message, sizeof(message), fp) != NULL){}
-     	    	fclose(fp);
-    	    
-	    	char *binmessage = (char *) malloc(sizeof(message)*8);
-    	    	strtobin(binmessage ,message);
-    
-    	    	printf("Vlozte kluc: ");
-    	    	scanf("%s", name);
-	    	sendmsg(binmessage, name);
-		free(binmessage);
-	    break;
+                //program is to send data. If source file is -, open stdin
+                //XXX:Je takyto STDIN standard?
+                config->input_file = set_member(optarg, strlen(optarg));
+                option_flags += SEND_FLAG;
+	        break;
+
             case 'r':
-		printf("Vlozte kluc: ");
-    	    	scanf("%s", name);
-		readmsg(name);
-                break;                      
+                config->output_file = set_member(optarg, strlen(optarg));
+                option_flags += READ_FLAG;
+                break;
+            default:
+                help();
+                return(EXIT_FAILURE);
         }
-   }
+    }
+ //   printf("%d",(int)(option_flags))
+    if((option_flags & READ_FLAG) == READ_FLAG && (option_flags & SEND_FLAG) == SEND_FLAG){
+        printf("You can either send or recieve, your options do not make sense\n");
+        help();
+        return(EXIT_FAILURE);
+    }
+    
+    read_conf_file();
+    if((option_flags & SEND_FLAG) == SEND_FLAG){
+        char *message = calloc(140, sizeof(char));
+        if (strcmp(config->input_file,"-") == 0){
+            printf("Type in your message (end with ^D):  ");
+            fp = stdin;
+            }else{
+            fp = fopen(config->input_file, "r");
+            }
+
+        if (fp == NULL){
+            printf("Can't open input file: %s\n", config->input_file);
+            return(EXIT_FAILURE);
+        }
+        printf("Sending Data...\n");
+        int *byte = calloc(1,sizeof(int));
+        int check = 1;
+        while(check){
+            check = fread(byte, 1, 1, fp);
+            send_data(byte[0]);
+        }
+
+        fclose(fp);
+        free(message);
+        printf("Data sent successfuly\n");
+        free(byte);
+        free_conf(config);
+        return(EXIT_SUCCESS);
+    }
+    if((option_flags & READ_FLAG) == READ_FLAG){
+        printf("Retrieving data...\n");
+        calibrate();
+        retrieve_msg();
+        free_sample_times();
+        free_conf(config);
+        return(EXIT_SUCCESS);
+    } 
+    help();
+    free_conf(config);
+    /*printf("input_file %s", config->input_file);
+    printf("name_server %s", config->name_server);
+    printf("name_base %s", config->name_base);
+    printf("key %s", config->key);*/
     return (EXIT_SUCCESS);
 }
