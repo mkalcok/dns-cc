8c13602e0f1b592001d2eb015775c621b8ddc4e9
Martin Kalcok <martin.kalcok@gmail.com>
2016-04-21 12:12:31 +0200

    CRC16 checksum

--- src/libs/crc.h
+++ src/libs/crc.h
@@ -4,8 +4,10 @@
 
 #ifndef DNS_CC_CRC_H
 #define DNS_CC_CRC_H
+
 #include <stdint.h>
 #include <stddef.h>
 
 uint32_t crc32(uint32_t crc, const void *buf, size_t size);
+uint16_t crc16(uint16_t crc, const void *buf, size_t size);
 #endif //DNS_CC_CRC_H
